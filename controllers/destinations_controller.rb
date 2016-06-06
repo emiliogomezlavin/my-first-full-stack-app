c65f4fcda668850b792e3ce6a6117fa28019ad35
Emilio Gomez Lavin <emiliogomezlavin@Emilios-MacBook-Pro.local>
2016-06-05 18:16:30 -0700

    first commit. Adds web app functionality with all pages working

---
+++ controllers/destinations_controller.rb
@@ -0,0 +1,58 @@
+class Travelution < Sinatra::Base
+
+  # RESTful Destination Controller Actions
+  # index
+  get '/destinations' do
+    # @artists = Artist.all
+    # erb(:"artists/index")
+  end
+
+  # new
+  get '/destinations/new' do
+    # @artist = Artist.new
+    # erb(:"artists/new")
+  end
+
+  # create
+  post '/destinations' do
+    # @artist = Artist.new(params[:artist])
+    # if @artist.save
+    #   redirect("/artists/#{@artist.id}")
+    # else
+    #   erb(:"artists/new")
+    # end
+  end
+
+  # show
+  get '/destinations/:id' do
+    # @artist = Artist.find(params[:id])
+    # erb(:"artists/show")
+  end
+
+  # edit
+  get '/destinations/:id/edit' do
+    # @artist = Artist.find(params[:id])
+    # erb(:"artists/edit")
+  end
+
+  # update
+  put '/destinations/:id' do
+    # @artist = Artist.find(params[:id])
+    # if @artist.update_attributes(params[:artist])
+    #   redirect("/artists/#{artist.id}")
+    # else
+    #   erb(:"artists/edit")
+    # end
+  end
+
+  # delete
+  delete '/destinations/:id/delete' do
+    # @artist = Artist.find(params[:id])
+    # if @artist.destroy
+    #   redirect('/artists')
+    # else
+    #   redirect("/artists/#{@artist.id}")
+    # end
+  end
+
+end
