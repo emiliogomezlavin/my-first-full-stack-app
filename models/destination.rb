fc0cce1e9095c97c8ce733e0de2681d990edea6d
Emilio Gomez Lavin <emiliogomezlavin@Emilios-MacBook-Pro.local>
2016-06-09 12:27:37 -0700

    uncomments the geocoder extention

--- models/destination.rb
+++ models/destination.rb
@@ -1,5 +1,5 @@
 class Destination < ActiveRecord::Base
-	# extend Geocoder::Model::ActiveRecord
- #  	geocoded_by :location
- #  	after_validation :geocode #fetches the coordinates
+	extend Geocoder::Model::ActiveRecord
+  	geocoded_by :location
+  	after_validation :geocode #fetches the coordinates
 end
