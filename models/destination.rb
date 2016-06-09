d839dee478b7af8a79efa34e592c69a26fc6dc60
Emilio Gomez Lavin <emiliogomezlavin@Emilios-MacBook-Pro.local>
2016-06-09 11:50:02 -0700

    everything ready before heroku hosting

--- models/destination.rb
+++ models/destination.rb
@@ -1,5 +1,4 @@
 class Destination < ActiveRecord::Base
-	#.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
 	extend Geocoder::Model::ActiveRecord
   	geocoded_by :location
   	after_validation :geocode #fetches the coordinates
